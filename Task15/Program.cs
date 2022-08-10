db282e450de8a3f56be5c49e7dc92d7e91f2f41f
Elizaveta <lizasid@mail.ru>
2022-08-10 17:06:04 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("No, this is not your day off, it's a working day.");
+        break;
+    case 2:
+        Console.WriteLine("No, this is not your day off, it's a working day.");
+        break;
+    case 3:
+        Console.WriteLine("No, this is not your day off, it's a working day.");
+        break;
+    case 4:
+        Console.WriteLine("No, this is not your day off, it's a working day.");
+        break;
+    case 5:
+        Console.WriteLine("No, this is not your day off, it's a working day.");
+        break;
+    case 6:
+        Console.WriteLine("Yes, this is your day off - Saturday!");
+        break;
+    case 7:
+        Console.WriteLine("Yes, this is your day off - Sunday!");
+        break;
+    default:
+        Console.WriteLine("There is no number like that. Try again.");
+        break;
+}
+
